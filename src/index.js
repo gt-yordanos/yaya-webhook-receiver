607e3247603fe8e9405890286ce9d0d5b9aa5515
gt-yordanos <gt.yordanos@gmail.com>
2025-08-24 18:18:12 +0300

    Refactor index.js: add HTTPS middleware and webhook router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
+import webhookRoutes from "./Routes/webhook.js";
+// import { httpsOnly } from "./Middlewares/httpsOnly.js";
 
 dotenv.config();
 const app = express();
 
-// Middleware to parse application/json
-app.use(bodyParser.json());
+// Enforce HTTPS middleware  - Commented Out for test on localhost
+// app.use(httpsOnly);
 
+app.use(cors()); // Enable CORS for all routes
+app.use(bodyParser.json()); // for application/json
+
+// Routes
+app.use("/webhook", webhookRoutes);
+
+// Health check
 app.get("/", (req, res) => {
   res.send("YaYa Wallet Webhook Receiver is running");
 });
